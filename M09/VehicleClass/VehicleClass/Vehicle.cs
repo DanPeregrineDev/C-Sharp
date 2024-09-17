e24886a69d08755b089b1de277274e7e5deabf3f
DanPeregrineDev <madeira.daf@gmail.com>
2024-09-17 13:09:14 +0100

    Updated

---
+++ M09/VehicleClass/VehicleClass/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleClass
+{
+    internal class Vehicle
+    {
+        // Caracteristics
+        string Brand;
+        string Model;
+        string Color;
+        string Plate;
+
+        int Year_Assembled;
+        int HorsePower;
+        int CylinderCapacity;
+        int NumberOfDoors;
+
+        // Behavior
+    }
+}
