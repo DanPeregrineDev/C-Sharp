51c53df0d04b23c5c38ed16cae9f39c9294e76ae
DanPeregrineDev <madeira.delta@gmail.com>
2024-10-10 15:44:23 +0100

    Ex2 Desafio 3

---
+++ M09/Desafio3/SquareEx2/SquareEx2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareEx2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square1 = new Square(8);
+
+            Console.WriteLine(Square.Area(square1));
+            Console.WriteLine(Square.Diagonal(square1));
+            Console.WriteLine(Square.Perimiter(square1));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ M09/Desafio3/SquareEx2/SquareEx2/Square.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareEx2
+{
+    internal class Square
+    {
+        // Properties
+        double side;
+
+        // Constructors
+        public Square(double side=1)
+        {
+            if (side <= 20 & side > 0)
+            {
+                this.side = side;
+            }
+        }
+
+        public static double Area(Square square)
+        {
+            return square.side * square.side;
+        }
+
+        public static double Diagonal(Square square)
+        {
+            return Math.Sqrt(2) * square.side;
+        }
+
+        public static double Perimiter(Square square)
+        {
+            return square.side * 4;
+        }
+    }
+}
