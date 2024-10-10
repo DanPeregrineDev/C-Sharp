731fe7086eb50f21e6dd003e409c3d0b48764709
DanPeregrineDev <madeira.delta@gmail.com>
2024-10-10 14:48:47 +0100

    Ex1 Desafio 3

---
+++ M09/Desafio3/PersonEx1/PersonEx1/Person.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonEx1
+{
+    internal class Person
+    {
+        // Properties
+        string name;
+        string city;
+
+        int age;
+
+        public static void showPerson(Person person)
+        {
+            Console.WriteLine($"Name: {person.name}\nAge: {person.age}\nCity: {person.city}\n");
+        }
+
+        // Constructor
+        public Person(string name, string city, int age)
+        {
+            this.name = name;
+            this.city = city;
+            this.age = age;
+        }
+    }
+}
---
+++ M09/Desafio3/PersonEx1/PersonEx1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonEx1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person newPerson1 = new Person("Ben", "Albany", 32);
+            Person newPerson2 = new Person("John", "Atlanta", 21);
+
+            Person.showPerson(newPerson1);
+            Person.showPerson(newPerson2);
+
+            Console.ReadLine();
+        }
+    }
+}
